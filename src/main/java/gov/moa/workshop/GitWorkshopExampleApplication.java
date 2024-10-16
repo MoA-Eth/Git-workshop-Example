d23a20d39283eeeb60ba98666b582b565f7ba02c
Daniel Fikre <dannyfiker@gmail.com>
2024-10-16 14:40:37 +0300

    Git 2nd workshop first commit

---
+++ src/main/java/gov/moa/workshop/GitWorkshopExampleApplication.java
@@ -0,0 +1,13 @@
+package gov.moa.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWorkshopExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitWorkshopExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gov/moa/workshop/GitWorkshopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.moa.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorkshopExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
